874854cf3af8c1f18bfecbb1ed8952345f8c0379
GregHilston <Gregory.Hilston@gmail.com>
2016-03-26 23:05:33 -0400

    Removed user input needed for adding repo

--- install_packages.sh
+++ install_packages.sh
@@ -18,14 +18,14 @@ echo "and extension pack here https://www.virtualbox.org/wiki/Downloads"
 # NOT WORKING ln -s /usr/share/applications/guake.desktop /etc/xdg/autostart/ (http://askubuntu.com/questions/368705/how-to-make-guake-start-at-login)
 
 #Configure git
-git config --global user.email "Gregory.Hilston@gmail.com"
+	git config --global user.email "Gregory.Hilston@gmail.com"
 git config --global user.name "GregHilston"
 
 # Install Cinnamon, Java 8, Sublime Text 3, Pidgin and Skype
 add-apt-repository ppa:lestcape/cinnamon 											# Install Cinnamon (http://www.webupd8.org/2014/12/install-cinnamon-24-stable-in-ubuntu.html)
-sudo add-apt-repository ppa:webupd8team/java 										# Install Java 8 (http://askubuntu.com/questions/634082/how-to-install-android-studio-on-ubuntu)
-add-apt-repository ppa:webupd8team/sublime-text-3 								 	# Install Sublime Text 3 (http://askubuntu.com/questions/172698/how-do-i-install-sublime-text-2-3)
-sudo add-apt-repository ppa:pidgin-developers/ppa
+sudo add-apt-repository -y ppa:webupd8team/java 										# Install Java 8 (http://askubuntu.com/questions/634082/how-to-install-android-studio-on-ubuntu)
+add-apt-repository -y ppa:webupd8team/sublime-text-3 								 	# Install Sublime Text 3 (http://askubuntu.com/questions/172698/how-do-i-install-sublime-text-2-3)
+sudo add-apt-repository -y ppa:pidgin-developers/ppa
 add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" 	# Install Skype (http://askubuntu.com/questions/293693/how-to-install-skype-with-ubuntu-13-04)#
 apt-get update
 apt-get install -y cinnamon oracle-java8-installer oracle-java8-set-default sublime-text-installer pidgin skype
